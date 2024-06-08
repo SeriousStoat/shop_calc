733f39175c2b32498c905800a46e824731439e18
SeriousStoat <aseriousstoat@pm.me>
2024-06-07 23:47:29 -0700

    created initial python file

---
+++ shop_calc.py
@@ -0,0 +1,7 @@
+""" Welcome,
+This is a calculator program designed to aid machinists
+by applying various formulas to requested info.
+"""
+import math
+
+print("The sin of 60 is " + str(math.sin(60)))
