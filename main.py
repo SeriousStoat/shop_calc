3fae219a4c00f8409c4c0d5c7f2750903e1d2216
SeriousStoat <aseriousstoat@pm.me>
2024-06-21 21:03:12 -0700

    Removed menu rounded corners

--- main.py
+++ main.py
@@ -6,32 +6,26 @@ from calc import Rpm, Sf
 
 class Header(CTkFrame):
     def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        self.label = CTkLabel(master=self, text="Welcome to My Shop Calculator",\
-                              font=("Tahoma", 30), text_color="orange")
-        self.label.grid(row=0, column=0, padx=55, sticky="nsew")
-        self.desc = CTkLabel(master=self, justify="left",\
-                              text="This application is for helping with machine shop calculations"\
-                             "\nPlease select an option to start calculating")
-        self.desc.grid(row=1, column=0, padx=55, pady=10, sticky=(W))
+        super().__init__(master, **kwagit rgs)
+        self.label = CTkLabel(master=self, text="My Shop Calculator", font=("Tahoma", 30), text_color="orange")
+        self.label.grid(row=0, column=0, padx=55, pady=20, sticky="nsew")
 
 class Main(CTk):
     def __init__(self):
         super().__init__()
         # Window and Theme Settings
         self.title("Shop Calculator")
-        self.geometry("600x300+300+200")
-        self.configure(fg_color="#181b1f", )
+        self.geometry("600x400+300+200")
+        self.configure(fg_color="#181b1f")
+        self.columnconfigure(1, weight=1)
         # Heading
-        self.header = Header(master=self)
-        self.header.grid(row=0, column=0, padx=20, pady=5, sticky="nsew")
+        self.header = Header(master=self, corner_radius=0)
+        self.header.grid(row=0, column=0, columnspan=3, pady=5, sticky="nsew")
         # Content
-        self.calcrpm = Rpm(master=self)
-        self.calcrpm.grid(row=1, column=0, padx=20, pady=5, sticky="new")
-        self.calcsf = Sf(master=self)
-        self.calcsf.grid(row=2, column=0, padx=20, pady=5, sticky="new")
-
-       
+        self.calcrpm = Rpm(master=self, corner_radius=0)
+        self.calcrpm.grid(row=1, column=0, columnspan=3, pady=5, sticky="new")
+        self.calcsf = Sf(master=self, corner_radius=0)
+        self.calcsf.grid(row=2, column=0, columnspan=3, pady=5, sticky="new")
 
 if __name__ == "__main__":
     app = Main()
