42eb9ee7a231c89a8fc3b7321f58ee701f9279d5
SeriousStoat <aseriousstoat@pm.me>
2024-07-19 10:25:26 -0700

    Changed import commands to only what is needed

--- msg.py
+++ msg.py
@@ -1,5 +1,5 @@
 # This module is separated so that it can interact with both the main app and modules
-from customtkinter import *
+from customtkinter import CTkFrame, CTkLabel, CTkFont
 
 class MsgBar(CTkFrame):# Content Frames
     def __init__(self, master, **kwargs):
